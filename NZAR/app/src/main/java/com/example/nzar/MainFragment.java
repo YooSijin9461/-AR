d296d7a4ec8b0be119d3f077f0cdb8ce86ffdefd
김소빈 <ksbjjang333@gmail.com>
2021-10-25 15:35:46 +0900

    [add] gitignore IDEA폴더 추가

--- NZAR/app/src/main/java/com/example/nzar/MainFragment.java
+++ NZAR/app/src/main/java/com/example/nzar/MainFragment.java
@@ -1,5 +1,13 @@
 package com.example.nzar;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -8,23 +16,23 @@ public class MainFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-//이 메소드가 호출될떄는 프래그먼트가 엑티비티위에 올라와있는거니깐 getActivity메소드로 엑티비티참조가능
+        //이 메소드가 호출될떄는 프래그먼트가 엑티비티위에 올라와있는거니깐 getActivity메소드로 엑티비티참조가능
         activity = (MainActivity) getActivity();
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-//이제 더이상 엑티비티 참초가안됨
+        //이제 더이상 엑티비티 참초가안됨
         activity = null;
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//프래그먼트 메인을 인플레이트해주고 컨테이너에 붙여달라는 뜻임
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main , container, false);
-        Button button = rootView.findViewById(R.id.button1);
+        //프래그먼트 메인을 인플레이트해주고 컨테이너에 붙여달라는 뜻임
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_save , container, false);
+        Button button = rootView.findViewById(R.id.back);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
