7b3b6623af7f05525bc930a3a550506b1400a139
neverte <neverte@naver.com>
2021-10-28 17:45:04 +0900

    [add] 카메라 촬영 영상 띄우기
    [add] 카메라 촬영 영상 띄우기

---
+++ Unity/NexonTest/Assets/Scripts/Camera.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Camera : MonoBehaviour
+{
+    private bool camAvailable;
+    private WebCamTexture backCam;
+    private Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    void Start() {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+    // 카메라 없다
+        if(devices.Length == 0) {
+            Debug.Log("no camera detected");
+            camAvailable = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if(!devices[i].isFrontFacing){
+                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+
+    // 백 캠을 못찾음
+        if(backCam == null){
+            Debug.Log("Unable to find back camera");
+            return;
+        }
+        backCam.Play();
+        background.texture = backCam;
+    }
+
+
+    void Update()
+    {
+        if(!camAvailable){
+            return;   
+        }
+
+        float ratio = (float)backCam.width / (float)backCam.height;
+    fit.aspectRatio = ratio;
+
+    float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
+
+    background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+    int orient = -backCam.videoRotationAngle;
+    background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+  } 
+}
