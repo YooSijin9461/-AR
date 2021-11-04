88877ec0a9d6edd14135030c81ea49ce7afe9e76
neverte <neverte@naver.com>
2021-11-04 16:30:30 +0900

    [add] 폴더 별 리스트 띄우기
    [add] 폴더 별 리스트 띄우기

--- Unity/NexonTest/Assets/Scripts/ARFilterManager.cs
+++ Unity/NexonTest/Assets/Scripts/ARFilterManager.cs
@@ -7,55 +7,57 @@ using UnityEngine.UI;
 public class ARFilterManager : MonoBehaviour
 {
   
-    public GameObject parentUI; // 부모 객체
-    public GameObject filterItem; //추가할 요소
-    // Start is called before the first frame update
-    void Start()
+  public GameObject parentUI; // 부모 객체, content
+  private GameObject contentTotal; //전체
+  private GameObject contentKart; //카트라이더
+  public GameObject filterItem; //추가할 요소
+
+  void Start()
     {
 
-    // 빌드 세팅에 올라간 씬 목록 가져오기
-    // int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;     
-    // string[] scenes = new string[sceneCount];
-    // for( int i = 0; i < sceneCount; i++ )
-    // {
-    //     scenes[i] = System.IO.Path.GetFileNameWithoutExtension( UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex( i ) );
-    //     Debug.Log(i + " :" + scenes[i]);
-    // }
+    contentTotal = GameObject.Find("ContentTotal").gameObject;
+    contentKart = GameObject.Find("ContentKart").gameObject;
 
-    DirectoryInfo dir = new DirectoryInfo(Application.dataPath + "/Scenes/SceneTypeOne");
+    DirectoryInfo dir = new DirectoryInfo(Application.dataPath + "/Scenes"); ///Scenes/SceneTypeOne
     FileInfo[] info = dir.GetFiles("*.unity");
-    foreach (FileInfo f in info){
-      // Scene이름.unity에서 .unity를 때는 작업.
-      Debug.Log(f.Name+" @@@@@@@@@@@@"+f.Name.ToString().Substring(0, f.Name.ToString().LastIndexOf(".")));
-    }
 
-    // 파일 길이 만큼 AssetItem 생성
+    // 파일 길이 만큼 AssetItem 생성해서 Total에 넣어줌
     foreach (FileInfo f in info){
       GameObject child = Instantiate(filterItem);
-      
-
-      child.transform.SetParent(parentUI.transform);
+      child.transform.SetParent(contentTotal.transform);
       // 버튼 내 text 변경
       child.GetComponentInChildren<Text>().text = f.Name.ToString().Substring(0, f.Name.ToString().LastIndexOf("."));
-      // GameObject textObject = child.transform.FindChild("Text").gameObject;
-      // Text text = textObject.transform.GetChild(0);
-      // textObject.getComponent<Text>().text = "hahah";
-
     }
 
-    // GameObject filterItemPrefab;
-    // foreach (FileInfo f in info){
-    //   filterItemPrefab = Resources.Load<GameObject>("FilterItem");
-    //   if (filterItemPrefab ==null)
-    //   {
-    //       Debug.Log("filterItemPrefab==null"); 
-    //   }
-    // }
+    dir = new DirectoryInfo(Application.dataPath + "/Scenes/SceneTypeOne"); ///Scenes/SceneTypeOne
+    info = dir.GetFiles("*.unity");
+
+    // 파일 길이 만큼 AssetItem 생성해서 kart 넣어줌
+    foreach (FileInfo f in info){
+      GameObject child = Instantiate(filterItem);
+      child.transform.SetParent(contentKart.transform);
+      // 버튼 내 text 변경
+      child.GetComponentInChildren<Text>().text = f.Name.ToString().Substring(0, f.Name.ToString().LastIndexOf("."));
+    }
+
   }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    //전체 목록 출력
+    public void showTotalList() {
+    Debug.Log("showKartList");
+    contentTotal.SetActive(true);
+    contentKart.SetActive(false);
+  }
+    //카트 목록 출력
+    public void showKartList() {
+    Debug.Log("showKartList");
+    contentTotal.SetActive(false);
+    contentKart.SetActive(true);
+  }
+
 }
